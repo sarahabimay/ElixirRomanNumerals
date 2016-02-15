e38697051312a7fcc4527ef0dd1e9fc60fda41ef
sarahabimay <sarahabimay@gmail.com>
2016-02-15 18:57:24 +0000

    First pattern generalized for 1, 2, 3

--- roman_numerals/test/roman_numerals_test.exs
+++ roman_numerals/test/roman_numerals_test.exs
@@ -2,7 +2,19 @@ defmodule ROMAN_NUMERALSTest do
   use ExUnit.Case
   doctest ROMAN_NUMERALS
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "convert 0 to ''" do
+    assert ROMAN_NUMERALS.convert(0) == ""
+  end
+
+  test "convert 1 to I" do
+    assert ROMAN_NUMERALS.convert(1) == "I"
+  end
+
+  test "convert 2 to II" do
+    assert ROMAN_NUMERALS.convert(2) == "II"
+  end
+
+  test "convert 3 to III" do
+    assert ROMAN_NUMERALS.convert(3) == "III"
   end
 end
