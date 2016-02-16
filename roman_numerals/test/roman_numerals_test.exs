9f1f9d95d9c72d3840ed94d6e9f191d1161e46a3
sarahabimay <sarahabimay@gmail.com>
2016-02-16 13:42:47 +0000

    Add remaining arabic to roman conversions and one mixed roman test.

--- roman_numerals/test/roman_numerals_test.exs
+++ roman_numerals/test/roman_numerals_test.exs
@@ -37,4 +37,36 @@ defmodule RomanNumeralsTest do
   test "convert 10 to X" do
     assert RomanNumerals.convert(10) == "X"
   end
+
+  test "convert 50 to L" do
+    assert RomanNumerals.convert(50) == "L"
+  end
+
+  test "convert 90 to XC" do
+    assert RomanNumerals.convert(90) == "XC"
+  end
+
+  test "convert 100 to C" do
+    assert RomanNumerals.convert(100) == "C"
+  end
+
+  test "convert 400 to CD" do
+    assert RomanNumerals.convert(400) == "CD"
+  end
+
+  test "convert 500 to D" do
+    assert RomanNumerals.convert(500) == "D"
+  end
+
+  test "convert 900 to CM" do
+    assert RomanNumerals.convert(900) == "CM"
+  end
+
+  test "convert 1000 to M" do
+    assert RomanNumerals.convert(1000) == "M"
+  end
+
+  test "convert 3999 to MMMCMXCIX" do
+    assert RomanNumerals.convert(3999) == "MMMCMXCIX"
+  end
 end
