c72583233978f4a34bf6178c42e3ef1efc15ed23
sarahabimay <sarahabimay@gmail.com>
2016-02-16 14:39:22 +0000

    Make most methods private

--- roman_numerals/lib/roman_numerals.ex
+++ roman_numerals/lib/roman_numerals.ex
@@ -18,15 +18,15 @@ defmodule RomanNumerals do
     convert(number, @arabic_romans_map)
   end
 
-  def convert(number, _) when number<1 do
+  defp convert(number, _) when number<1 do
     ""
   end
 
-  def convert(number, [[roman, arabic] | _] = roman_arabic_map) when number>=arabic do
+  defp convert(number, [[roman, arabic] | _] = roman_arabic_map) when number>=arabic do
     roman <> convert(number-arabic, roman_arabic_map)
   end
 
-  def convert(number, [_ | numerals_shifted]) do
+  defp convert(number, [_ | numerals_shifted]) do
     convert(number, numerals_shifted)
   end
 end
